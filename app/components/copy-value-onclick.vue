df24efafc88cddd315efeef4c2ba7a3aa2c2c23f
rexpl <75545403+rexpl@users.noreply.github.com>
2025-12-23 20:56:35 +0100

    ui improvments

--- app/components/copy-value-onclick.vue
+++ app/components/copy-value-onclick.vue
@@ -35,7 +35,7 @@ async function copy() {
         <div>
             <slot />
         </div>
-        <div class="hidden group-hover:block">
+        <div class="invisible group-hover:visible">
             <svg v-if="status === CopyStats.Available" class="w-4 h-4 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-6 5h6m-6 4h6M10 3v4h4V3h-4Z"/>
             </svg>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -94,11 +94,11 @@ useHead({
             </ul>
 
             <div class="flex items-center justify-between border-t border-gray-200 dark:border-gray-800">
-                <label class="flex items-center cursor-pointer border-r border-gray-200 dark:border-gray-800 p-4">
+                <label for="theme-selector" class="flex items-center cursor-pointer border-r border-gray-200 dark:border-gray-800 p-4">
                     <svg id="theme-toggle-light-icon" class="w-5 h-5" :class="dark ? '' : 'text-orange-600'" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
                     </svg>
-                    <input type="checkbox" v-model="dark" class="sr-only peer" @change="toggleDarkMode">
+                    <input id="theme-selector" type="checkbox" v-model="dark" class="sr-only peer" @change="toggleDarkMode">
                     <span class="relative mx-3 w-9 h-5 bg-gray-200 dark:bg-gray-950 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-gray-400 dark:after:bg-gray-900 after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand"></span>
                     <svg id="theme-toggle-dark-icon" class="w-5 h-5" :class="dark ? 'text-orange-600' : ''" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
@@ -118,7 +118,7 @@ useHead({
         </div>
     </aside>
 
-    <div class="p-4 ml-56">
+    <main class="p-4 ml-56">
         <slot />
-    </div>
+    </main>
 </template>
