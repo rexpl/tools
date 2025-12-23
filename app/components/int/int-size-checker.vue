60c7c74656feaf76895848cd786217ed825507da
rexpl <75545403+rexpl@users.noreply.github.com>
2025-12-23 08:56:24 +0100

    autofocus int tooling

--- app/components/int/int-size-checker.vue
+++ app/components/int/int-size-checker.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {fitsIn, IntSize, toBigInt, sizeLabels} from "~~/src/int";
-import {watch} from "vue";
+import {watch, onMounted} from "vue";
 
 const input = ref<string>('');
 
@@ -44,6 +44,11 @@ const blocksOrder: IntSize[] = [
     IntSize.Int8, IntSize.Int16, IntSize.Int32, IntSize.Int64, IntSize.Int128,
     IntSize.UInt8, IntSize.UInt16, IntSize.UInt32, IntSize.UInt64, IntSize.UInt128,
 ];
+
+const numberInput = ref<HTMLInputElement>();
+onMounted(() => {
+    numberInput.value?.focus();
+});
 </script>
 
 <template>
@@ -53,6 +58,7 @@ const blocksOrder: IntSize[] = [
                 type="text"
                 name="input"
                 autocomplete="off"
+                ref="numberInput"
                 v-model="input"
                 :class="validNumber ? 'border-gray-200 dark:border-gray-800 focus:ring-orange-600 focus:border-orange-600' : 'border-red-600 focus:ring-red-600'"
                 class="w-full max-w-xl px-3 py-2.5 bg-gray-100 dark:bg-gray-900 border text-heading text-sm rounded-md shadow-xs"
@@ -74,7 +80,7 @@ const blocksOrder: IntSize[] = [
             <svg v-if="bitSize > 0 && !valueFits![intSize]" class="w-10 h-10 p-1 text-red-600 bg-red-200 rounded-full" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
             </svg>
-            <svg v-if="bitSize === 0" class="w-10 h-10 p-1 text-gray-600 bg-gray-200 rounded-full" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <svg v-if="bitSize === 0" class="w-10 h-10 p-1 text-gray-600 dark:text-gray-400 bg-gray-200 dark:bg-gray-700 rounded-full" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.529 9.988a2.502 2.502 0 1 1 5 .191A2.441 2.441 0 0 1 12 12.582V14m-.01 3.008H12M21"/>
             </svg>
 
