0b579f1d110a84a0f07115deaeb2e92134f1addc
rexpl <75545403+rexpl@users.noreply.github.com>
2025-12-23 09:08:35 +0100

    feat: base 64 utils

--- app/pages/base64.vue
+++ app/pages/base64.vue
@@ -2,8 +2,33 @@
 import ToolHeader from "~/components/tool-header.vue";
 import ToolHeaderHighlight from "~/components/tool-header-highlight.vue";
 import SplitLayout from "~/components/ui/split-layout.vue";
+import {onMounted} from "vue";
+import {baseUrl, makeTitle} from "~~/src/constants";
+import logo from "~/assets/img/logo.png";
 
-const horizontal = ref(false);
+const title = 'Base 64';
+const description = 'Provides basic base 64 encoding/decoding with live editing.';
+
+const pageTitle = makeTitle(title);
+useSeoMeta({
+    title: pageTitle,
+});
+
+if (import.meta.server) {
+    useSeoMeta({
+        description: description,
+        ogTitle: pageTitle,
+        ogDescription: description,
+        ogImage: baseUrl(logo),
+        ogUrl: baseUrl(`/base64`),
+        ogType: 'website',
+        ogSiteName: pageTitle,
+        twitterCard: 'summary_large_image',
+        twitterTitle: pageTitle,
+        twitterDescription: description,
+        twitterImage: baseUrl(logo),
+    });
+}
 
 const base64 = ref('');
 const rawtext = ref('');
@@ -22,22 +47,25 @@ function encode(value: string): string {
 </script>
 
 <template>
-    <ToolHeader description="Provides basic base 64 encoding/decoding with live editing.">
-        Encode/Decode <ToolHeaderHighlight title="Base 64" /> Data
+    <ToolHeader :description="description">
+        Encode/Decode <ToolHeaderHighlight :title="title" /> Data
     </ToolHeader>
 
-    <div class="w-full h-[calc(100vh-137px)]">
-        <SplitLayout local-storage="layout:base64" :initial-ratio="0.40">
-            <template #first>
+    <!-- without this causes hydration mismatches as the split layout uses local storage unavailable at build -->
+    <ClientOnly>
+        <div class="w-full h-[calc(100vh-137px)]">
+            <SplitLayout local-storage="layout:base64" :initial-ratio="0.40">
+                <template #first>
                 <textarea id="message" rows="4" class="bg-gray-50 dark:bg-gray-900 border border-gray-200 dark:border-gray-800 text-heading text-sm rounded-md focus:ring-0 block w-full h-full p-3 shadow-xs"
                           placeholder="Base 64 data" v-model="base64" @input="rawtext = decode(base64)" ref="base64Input"></textarea>
-            </template>
-            <template #second>
+                </template>
+                <template #second>
                 <textarea id="message" rows="4" class="bg-gray-50 dark:bg-gray-900 border border-gray-200 dark:border-gray-800 text-heading text-sm rounded-md focus:ring-0 block w-full h-full p-3 shadow-xs"
                           placeholder="Decoded data" v-model="rawtext" @input="base64 = encode(rawtext)"></textarea>
-            </template>
-        </SplitLayout>
-    </div>
+                </template>
+            </SplitLayout>
+        </div>
+    </ClientOnly>
 </template>
 
 <style scoped>
